9b429f4f7492aa4096083523704409bad1519fd8
Adel Khedhiri <khedhiri.adel@gmail.com>
2025-02-27 19:04:46 +0100

    update2

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./views/Home";
-import Shop from "./views/Shop";
+
 import Cart from "./views/Cart";
-import Orders from "./views/Orders";
-import AdminDashboard from "./views/AdminDashboard";
+
+
 import Login from "./views/Login";
 import Register from "./views/Register";
 import Navbar from "./components/Navbar";
@@ -14,11 +13,8 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+       
         <Route path="/cart" element={<Cart />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
